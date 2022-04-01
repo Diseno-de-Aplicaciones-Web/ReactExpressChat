2ea9701573f8c0bb79f16296882fd7f022cced1d
ohmelopido@gmail.com <afdm@postal6.academiapostal.es.local>
2022-04-01 13:48:36 +0200

    GET/users/messages

--- src/PoC/index.mjs
+++ src/PoC/index.mjs
@@ -2,6 +2,9 @@ const host = "https://web-develop-react-express-chat.herokuapp.com"
 const htmlGetUsers = document.querySelector("#getUsers");
 const htmlUpdateButton = document.querySelector("#updateButton");
 
+const htmlGetMessages = document.querySelector("#getMessages");
+const htmlUpdateBoton = document.querySelector("#updateBoton");
+
 async function get(url) {
     const response = await fetch(url);
     const data = await response.json();
@@ -18,3 +21,47 @@ function updateButtonClickHandler() {
 }
 
 htmlUpdateButton.addEventListener("click", updateButtonClickHandler)
+
+// Mi prueba
+
+function authToken(id, secret) {
+    // En autenticación Basic, usuario y contraseña se separan con ':'
+    const authToken = `${id}:${secret}`;
+    // Y se codifican en Base64
+    const base64token = btoa(authToken);
+    return `Basic ${base64token}`;
+}
+
+const miURL = host+"/messages/";
+let miToken = authToken(1648811234937, "abc123");
+
+/**
+ * GET con autenticación
+ */
+async function authGet(url, token) {
+    const response = await fetch(
+        url,
+        { 
+            headers: {
+                Authorization: token
+            }
+        }
+    );
+    const data = await response.json();
+    return data;
+}
+
+
+
+async function getMessages () {
+    const messages = await get(host+"/messages/");
+    htmlGetMessages.innerText = JSON.stringify(messages);
+};
+
+function updateBotonClickHandler() {
+    getMessages();
+}
+
+htmlUpdateBoton.addEventListener("click", updateBotonClickHandler)
+
+authGet(miURL, miToken);
